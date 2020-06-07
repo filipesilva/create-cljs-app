5ed3f115192e1dc3f34a0c080ec30a47f5d37549
Filipe Silva <filipematossilva@gmail.com>
2020-06-07 14:38:10 +0100

    feat: update reagent and devcards

--- template/src/app/core.cljs
+++ template/src/app/core.cljs
@@ -1,12 +1,12 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
-  (:require [reagent.core :as r]
+  (:require [reagent.dom :as rd]
             [app.hello :refer [hello]]))
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
   []
-  (r/render [hello] (.getElementById js/document "app")))
+  (rd/render [hello] (.getElementById js/document "app")))
 
 (defn ^:export main
   "Run application startup logic."
